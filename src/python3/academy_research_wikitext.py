0d33173b9a4cb9ddcd0e68a8a644448528f2aae1
Oscar de Groot <odegroot@gmail.com>
2011-12-20 02:38:39 +0100

    Added base icons to generated wikitext. Overlay icons are still wrong.

--- src/python3/academy_research_wikitext.py
+++ src/python3/academy_research_wikitext.py
@@ -11,24 +11,55 @@ Created on Dec 19, 2011
 
 import academy_research
 import collections
+import json
+import os
+
+__project_root = os.path.join('..', '..')
+__icon_name_map_path = os.path.join(__project_root, 'src', 'json', 'icon_name_map.json')
 
 def main():
     eng = academy_research.get_localization('eng')
     projects = academy_research.get_research_project_dicts(eng)
+    with open(__icon_name_map_path, encoding='utf_8') as file:
+        icon_name_map = json.load(file)['data']
     
     project_counts = get_counts_per_category(projects)
     
     category = None
     subcategory = None
+    table_open = False
     
     for project in projects:
         if project['category'] != category:
+            if table_open:
+                print('|}')
+                table_open = False
             category = project['category']
             print('\n=={} ({} projects)=='.format(category, project_counts[category]))
         
         if 'subcategory' in project and project['subcategory'] != subcategory:
+            if table_open:
+                print('|}')
+                table_open = False
             subcategory = project['subcategory']
-            print('\n==={}==='.format(subcategory))
+            print('\n==={}===\n'.format(subcategory))
+        
+        if not table_open:
+            print('{{Research-header}}\n')
+            table_open = True
+        
+        print('{{Research')
+        print('| stars = ' + str(project['ItemQuality.stars']))
+        
+        try:
+            base_icon_wiki_filename = icon_name_map[project['icon.base']]
+        except KeyError:
+            raise Exception('"{}": "{} item.png",'.format(project['icon.base'], project['affects.engs'][0]))
+        
+        print('| icon=[[File:{}]]<span style="margin-left: -46px;">[[File:Multiple effects.png]]</span>'.format(base_icon_wiki_filename))
+        print('}}\n')
+    
+    print('|}')
 
 def get_counts_per_category(projects):
     counts = collections.defaultdict(int)
