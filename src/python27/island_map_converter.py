294cc1748e45347c1a72fc9159234587540d3c4a
Peter Hozak <peter.hozak@gmail.com>
2012-01-02 02:35:12 +0100

    proposed algorithm for real use

--- src/python27/island_map_converter.py
+++ src/python27/island_map_converter.py
@@ -1,9 +1,13 @@
 '''
 Converts game data files for islands to custom data formats readable by building layout tools
-
+(v0.1 did not work, v0.2 is prove of concept only)
 
 Proposed algorithm:
- ?!?
+ 1. load data/levels/... scenario+campaign www files - ???
+ 2. for each www file load all islands
+ 3. for each island compute all builblocking polygons
+ 4. draw the polygons and save as png for debugging
+ 5. convert drawings to usable formats
  + one-time function to copy all related files do src/rda/...
 
  to do: look at import/export format for http://code.google.com/p/anno-designer/
@@ -33,7 +37,7 @@ except ImportError:
     print("To download PIL, see http://www.pythonware.com/products/pil/")
     raise
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 def _transform(rgb):
     # to do: create a mapping from actual .png colors ;))
@@ -72,9 +76,9 @@ def test():
     #print(polygon)
     png = Image.open("test.png")
     size = png.size[1]
-    polygon = [(x+10, size-y-24) for x, y in polygon]
+    polygon = [(x+8, size-y-24) for x, y in polygon]
     draw = ImageDraw.Draw(png)
-    draw.polygon(polygon, outline=(255,0,0))
+    draw.polygon(polygon, fill=(255,0,0))
     del draw
     png.save("result.png")
 
